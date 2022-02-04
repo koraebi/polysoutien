7c42a33e12efd3b07360a744b3133c8f608f892a
mehdi <mehdi.cherkaoui@etu.unice.fr>
2022-02-04 08:38:53 +0100

    update service

---
+++ WebFront/src/app/annoucements.mock.ts
@@ -0,0 +1,34 @@
+import { Announcement } from './announcement';
+
+export const ANNOUCEMENTS: Announcement[] = [
+    {
+        user: {
+          nom: "Cherkaoui",
+          prenom: "Mehdi",
+          filiere: "IHM-SI5",
+          image: "../assets/mehdi.jpg"
+        },
+        id: 1,
+        matiere: "C",
+        description: "Bonjour je suis étudiant en SI5 en IHM, je suis à l'aise en C et"+
+         "je pourrais vous aidez dans cette matière surout les SI3. Vous pouvez vous inscrire à l'annonce.",
+        salle: "O+302",
+        date: "31/01/2022 à 18h.",
+        duree: 1,
+      },
+      {
+        user: {
+          nom: "Filipe",
+          prenom: "Gomes",
+          filiere: "AL-SI5",
+          image: "../assets/filipe.PNG"
+        },
+        id: 2,
+        matiere: "Java",
+        description: "Bonjour je suis étudiant en SI5 en AL, "+
+         "je vous donne rendez-vous le 2 février. Vous pouvez vous inscrire à l'annonce.",
+        salle: "O+310",
+        date: "02/02/2022 à 14h.",
+        duree: 2,
+      }
+]
--- WebFront/src/app/announcement.service.ts
+++ WebFront/src/app/announcement.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ANNOUCEMENTS } from './annoucements.mock';
 import { Announcement } from './announcement';
 
 @Injectable({
@@ -7,38 +8,7 @@ import { Announcement } from './announcement';
 })
 export class AnnouncementService {
 
-  private annonces: Announcement[] = [
-    {
-      user: {
-        nom: "Cherkaoui",
-        prenom: "Mehdi",
-        filiere: "IHM-SI5",
-        image: "../assets/mehdi.jpg"
-      },
-      id: 1,
-      matiere: "C",
-      description: "Bonjour je suis étudiant en SI5 en IHM, je suis à l'aise en C et"+
-       "je pourrais vous aidez dans cette matière surout les SI3. Vous pouvez vous inscrire à l'annonce.",
-      salle: "O+302",
-      date: "31/01/2022 à 18h.",
-      duree: 1,
-    },
-    {
-      user: {
-        nom: "Amine",
-        prenom: "Amine",
-        filiere: "AL-SI5",
-        image: "../assets/image.jpg"
-      },
-      id: 2,
-      matiere: "Java",
-      description: "Bonjour je suis étudiant en SI5 en AL, "+
-       "je vous donne rendez-vous le 2 février. Vous pouvez vous inscrire à l'annonce.",
-      salle: "O+310",
-      date: "02/02/2022 à 14h.",
-      duree: 2,
-    }
-  ];
+  private annonces: Announcement[] = ANNOUCEMENTS;
   public annonces$: BehaviorSubject<Announcement[]> = new BehaviorSubject(this.annonces);
 
   constructor() { }
